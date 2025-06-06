d9f7e9b78d63c6741d28ba04897a75e7fe79e1bf
junjuny0227 <junjuny0227@gmail.com>
2025-06-07 02:56:31 +0900

    rewrites 설정

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
