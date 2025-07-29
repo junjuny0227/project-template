2d8533f2c5c80bbd4fde060f03d5684e72a1995f
junjuny0227 <junjuny0227@gmail.com>
2025-07-29 22:12:39 +0900

    feat: tanstack-query Provider 제작 및 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import Provider from "./provider";
 
 export const metadata: Metadata = {
   title: "project-title",
@@ -13,7 +14,9 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 };
