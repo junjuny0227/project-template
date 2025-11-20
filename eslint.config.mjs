3afeca3ffcd10c933265c5efdf3d392f6f2daf8b
junjuny0227 <junjuny0227@gmail.com>
2025-11-20 20:39:01 +0900

    Next.js v16 업데이트 및 React Compiler 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,45 +1,45 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettierConfig from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettierConfig,
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  globalIgnores([
+    'node_modules/**',
+    '.next/**',
+    'out/**',
+    'build/**',
+    'public/**',
+    'next-env.d.ts',
+    '**/*.d.ts',
+    'pnpm-lock.yaml',
+    'yarn.lock',
+    'package-lock.json',
+    '.DS_Store',
+  ]),
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
   {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'public/**',
-      'next-env.d.ts',
-      '**/*.d.ts',
-      'pnpm-lock.yaml',
-      'yarn.lock',
-      'package-lock.json',
-      '.DS_Store',
-    ],
-  },
-  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+
     settings: {
       react: {
         version: '19',
       },
     },
-  },
-  {
+
     rules: {
       'prettier/prettier': 'error',
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'error',
     },
   },
-];
+]);
 
 export default eslintConfig;
